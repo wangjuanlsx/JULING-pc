e8306e43f1e77d9d068621e80e48fb1cb01aa64b
sss <sss>
2019-05-26 09:11:38 +0800

    提交

--- js/kf-table.js
+++ js/kf-table.js
@@ -1,20 +1,26 @@
 $(function(){
+    // 如果ie9以下，用js翻转
+    if(navigator.appName == "Microsoft Internet Explorer"&&parseInt(navigator.appVersion.split(";")[1].replace(/[ ]/g, "").replace("MSIE",""))<10){
+        $('.kf-table-left-flip').css({'display':'none'});
+        $('#vertical').css({'display':'black'});
+    }else {
+        $('.kf-table-left-flip').css({'display':'block'});
+        $('#vertical').css({'display':'none'});
+    }
     //翻转
     var turn = function(target,time,opts){
         target.find('div.kf-table-left-item').hover(function(){
-            $(this).find('img.prev-img').css({'height':'100%'});
             $(this).find('img.prev-img').stop().animate(opts[0],time,function(){
                 $(this).hide().next().show();
                 $(this).next().animate(opts[1],time);
             });
         },function(){
-            $(this).find('.kf-table-left-turn').css({'height':'100%'});
             $(this).find('.kf-table-left-turn').animate(opts[0],time,function(){
                 $(this).hide().prev().show();
                 $(this).prev().animate(opts[1],time);
             });
         });
     }
-    var verticalOpts = [{'width':0},{'width':'110%'}];
+    var verticalOpts = [{'width':0},{'width':'236'}];
     turn($('#vertical'),180,verticalOpts);
 })
