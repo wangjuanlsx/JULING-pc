19390700a1053c998317beff7cdf1bd1e8ef1e48
sss <sss>
2019-05-29 22:22:59 +0800

    提交

--- js/kf-table.js
+++ js/kf-table.js
@@ -3,6 +3,7 @@ $(function(){
     if(navigator.appName == "Microsoft Internet Explorer"&&parseInt(navigator.appVersion.split(";")[1].replace(/[ ]/g, "").replace("MSIE",""))<10){
         $('.kf-table-left-flip').css({'display':'none'});
         $('#vertical').css({'display':'black'});
+        $('.fixed-bg-wrap').css({'top':'0'});
     }else {
         $('.kf-table-left-flip').css({'display':'block'});
         $('#vertical').css({'display':'none'});
@@ -23,4 +24,23 @@ $(function(){
     }
     var verticalOpts = [{'width':0},{'width':'370px'}];
     turn($('#vertical'),180,verticalOpts);
+    /*弹框*/
+    $('.kf-table-btn-animate-wrap').on('click', function(){
+        $('.mark').css({'display':'block'});
+        $('.mark-content-wrap').css({'display':'block'});
+    });
+    $('.mark-close-btn').on('click', function(){
+        $('.mark').css({'display':'none'});
+        $('.mark-content-wrap').css({'display':'none'});
+    });
+    /*滚动*/
+    $(document).on('scroll', function(){
+        var scrollHeight = $(document).scrollTop();
+        console.log("滚动",scrollHeight);
+        if(scrollHeight>900){
+            $('.fixed-bg-wrap').css({'top':'0'});
+        }else {
+            $('.fixed-bg-wrap').css({'top':'500px'});
+        }
+    })
 })
